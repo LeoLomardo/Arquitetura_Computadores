d9bc12dd3a06c6522df7d408ce72bc274709dfe9
Bernardo-PUC <bernardo.vs245@gmail.com>
2025-11-06 14:43:47 -0300

    Atualiza arquivos para T4 (.c -> .cu)

--- T4/matrix_lib.h
+++ T4/matrix_lib.h
@@ -4,7 +4,9 @@
 struct matrix {
     unsigned long int height;
     unsigned long int width;
-    float *rows;
+    float *h_rows;
+    float *d_rows;
+    int alloc_mode;
 };
 
 typedef struct {
@@ -17,6 +19,7 @@ typedef struct {
 
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix);
 int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct matrix *matrixC);
+int set_grid_size(int threads_per_block, int max_blocks_per_grid);
 void* matrix_mult_worker(void* args);
 void set_number_threads(int num_threads);
 
